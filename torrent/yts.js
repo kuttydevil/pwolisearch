d2fb0f7252941eb9af4aa3209bbfb764108532d7
kuttydevil <63119518+kuttydevil@users.noreply.github.com>
2025-12-15 12:53:40 +0300

    Update yts.js

--- torrent/yts.js
+++ torrent/yts.js
@@ -1,86 +1,59 @@
-const cheerio = require('cheerio');
 const axios = require('axios');
 
-
-
 async function yts(query, page = '1') {
+    // 1. Use the Official YTS API to avoid Cloudflare blocks
+    const url = `https://yts.mx/api/v2/list_movies.json?query_term=${query}&page=${page}`;
 
-    let all = []
-    let ALLURL = [];
-    if (page === '' || page === '1') {
-        var url = "https://yts.lt/browse-movies/" + query + "/all/all/0/latest/0/all"
-    } else {
-        var url = "https://yts.lt/browse-movies/" + query + "/all/all/0/latest/0/all?page=" + page;
-    }
-    let html;
     try {
-        html = await axios.get(url, headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"
-        });
-    } catch {
-        return null;
-    }
-
-    const $ = cheerio.load(html.data);
-    $('div.browse-movie-bottom').each((_, element) => {
-        let url = $(element).find('a').attr('href');
-        ALLURL.push(url);
-    })
-
-    await Promise.all(ALLURL.map(async (url) => {
-        const data = {
-            'Name': null,
-            'ReleasedDate': null,
-            'Genre': null,
-            'Rating': null,
-            'Likes': null,
-            'Runtime': null,
-            'Language': null,
-            'Url': null,
-            'Poster': null,
-            'Files': []
-        };
-        let html;
-        try {
-            html = await axios.get(url);
-        } catch {
-            return;
+        const response = await axios.get(url);
+        
+        if (!response.data || !response.data.data || !response.data.data.movies) {
+            return []; // Return an empty array if no movies are found
         }
 
-        const $ = cheerio.load(html.data);
+        const movies = response.data.data.movies;
+        let all = [];
 
-        data['Name'] = $('div.hidden-xs').find('h1').text();
-        data['ReleasedDate'] = $('div.hidden-xs').find('h2').eq(0).text();
-        data['Genre'] = $('div.hidden-xs').find('h2').eq(1).text();
-        data['Rating'] = (($('div.bottom-info div.rating-row').eq(3).find('span').eq(0).text()) + ' ⭐').trim() || 'Not Rated'
-        data['Likes'] = $('div.bottom-info div.rating-row').eq(0).find('span').eq(1).text()
-        data['Runtime'] = $('div .tech-spec-info').find('div .row').eq(1).find('div .tech-spec-element').eq(2).text().trim();
-        data['Language'] = $('div .tech-spec-info').find('div .row').eq(0).find('div .tech-spec-element').eq(2).text().trim();
-        data['Url'] = url;
-        data['Poster'] = $('div #movie-poster').eq(0).find('img').attr('src');
+        // Map the API data to the format your frontend expects
+        movies.forEach(movie => {
+            const trackers = "&tr=udp://open.demonii.com:1337/announce&tr=udp://tracker.openbittrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce";
 
-        $('.modal-download > div:nth-child(1) div.modal-content').each((i, el) => {
-            $('div.modal-torrent').each((_, ele) => {
-                let files = {};
-                files.Quality = $(ele).find(':nth-child(1) >span').text();;
-                files.Type = $(ele).find(':nth-child(2)').text();
-                files.Size = $(ele).find(':nth-child(5)').text();
-                files.Torrent = $(ele).find(':nth-child(6)').attr('href');
-                files.Magnet = $(ele).find(':nth-child(7)').attr('href');
+            const data = {
+                'Name': movie.title,
+                'ReleasedDate': movie.year.toString(),
+                'Genre': movie.genres ? movie.genres.join(', ') : 'N/A',
+                'Rating': `${movie.rating} ⭐`,
+                'Likes': 'N/A',
+                'Runtime': `${movie.runtime} min`,
+                'Language': movie.language,
+                'Url': movie.url,
+                'Poster': movie.medium_cover_image,
+                'Files': []
+            };
 
-                data.Files.push(files);
-            })
+            if (movie.torrents) {
+                movie.torrents.forEach(torrent => {
+                    data.Files.push({
+                        Quality: torrent.quality,
+                        Type: torrent.type,
+                        Size: torrent.size,
+                        Torrent: torrent.url,
+                        Magnet: `magnet:?xt=urn:btih:${torrent.hash}&dn=${encodeURIComponent(movie.title)}${trackers}`
+                    });
+                });
+            }
+            all.push(data);
+        });
 
+        return all;
 
-        })
-        all.push(data);
-    }))
-
-    return all;
-
-
-
+    } catch (error) {
+        console.error("YTS API Error:", error.message);
+        return null; // Let app.js handle the null response
+    }
 }
 
-
-module.exports = yts
+// 2. THIS IS THE FIX for "TypeError: scrapYts.yts is not a function"
+module.exports = {
+    yts: yts
+};
